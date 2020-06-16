e81ebdfd6d700a1b622d190a871e22188250ffde
Aniket Chauhan <AniketAK@hotmail.com>
2020-06-16 14:49:23 +0530

    half code for employee selection

---
+++ HRMS/ViewModel/ProgramEmployeeSelectionCommon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HRMS.Models;
+
+namespace HRMS.ViewModel
+{
+    public class ProgramEmployeeSelectionCommon
+    {
+        public HRMS_ProgramDetail programDetail { get; set; }
+        public HRMS_TrainingApproval employeeselection { get; set; }
+        public List<HRMS_TrainingApproval> EmoloyeedataList { get; set; }
+    }
+}
